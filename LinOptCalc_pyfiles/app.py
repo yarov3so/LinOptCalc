eb823e8d6c979652be4e40e875834e2fa65c5536
Yaro <61938366+yarov3so@users.noreply.github.com>
2025-03-22 19:23:16 -0400

    Update app.py

--- LinOptCalc_pyfiles/app.py
+++ LinOptCalc_pyfiles/app.py
@@ -1,6 +1,14 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
+st.title("Linear Optimization Calculator (LinOptCalc)")
+st.markdown("A versatile implementation of the simplex method for linear optimization that thoroughly documents every step in the optimization process.")
+
 iframe_code="""<iframe src="https://trinket.io/embed/python3/a57e81eabcb8?outputOnly=true&runOption=run&start=result" width="100%" height="400px" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen></iframe>"""
 
-components.html(iframe_code, height=600)
+components.html(iframe_code, height=10000)
+
+st.text("")
+st.markdown("""*Crafted by yarov3so*   
+<a href="https://www.buymeacoffee.com/yarov3so" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" style="width: 9em; height: auto; padding-top: 0.7em; padding-bottom: 1em" ></a>  
+See my other [Math Help Tools](https://mathh3lptools.streamlit.app)""",unsafe_allow_html=True)
