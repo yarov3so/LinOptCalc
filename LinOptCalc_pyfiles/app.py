c5a4876a1863d83dc17b784f67c7cf019e471b05
Yaro <61938366+yarov3so@users.noreply.github.com>
2025-04-04 10:21:59 -0400

    Update app.py

--- LinOptCalc_pyfiles/app.py
+++ LinOptCalc_pyfiles/app.py
@@ -8,7 +8,7 @@ st.set_page_config(
 
 st.title("Linear Optimization Calculator (LinOptCalc)")
 st.markdown("A versatile implementation of the simplex method for linear optimization that thoroughly documents every step in the optimization process.")
-st.markdown("**Note: when specifying the linear constraints, do NOT include the trivial non-negativity constraints.**")
+st.markdown("**Note: when specifying the linear constraints, do NOT include the trivial non-negativity ones.**")
 iframe_code="""<iframe src="https://trinket.io/embed/python3/dbf60d5f9ede?outputOnly=true&runOption=run&start=result" width="100%" height="600" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen></iframe>"""
 
 components.html(iframe_code, height=600)
