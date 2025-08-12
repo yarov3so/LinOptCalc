e9b79f00f0cf83790aaae341f53da3d19e1402b5
Yaro <61938366+yarov3so@users.noreply.github.com>
2025-08-12 10:10:23 -0400

    Update app.py

--- LinOptCalc_pyfiles/app.py
+++ LinOptCalc_pyfiles/app.py
@@ -4,7 +4,7 @@ import streamlit.components.v1 as components
 st.set_page_config(
     page_title="LinOptCalc",
     page_icon="MHT.png", 
-    layout="wide"
+    
 )
 
 st.title("Linear Optimization Calculator (LinOptCalc)")
@@ -12,7 +12,7 @@ st.markdown("A versatile implementation of the simplex method for linear optimiz
 st.markdown("**Note: when specifying the linear constraints, do NOT include the trivial non-negativity ones.**")
 iframe_code="""<iframe src="https://trinket.io/embed/python3/f4afde17d058?outputOnly=true&runOption=run&start=result" width="100%" height="600" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen></iframe>"""
 
-components.html(iframe_code)
+components.html(iframe_code, height=600, width=600)
 
 st.markdown("""*Crafted by yarov3so*   
 <a href="https://www.buymeacoffee.com/yarov3so" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" style="width: 9em; height: auto; padding-top: 0.7em; padding-bottom: 1em" ></a>  
